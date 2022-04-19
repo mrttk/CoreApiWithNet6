5caef1001b156e322e76f95f2fe1cdf683706070
= <=>
2022-04-19 23:31:06 +0300

    Add Entity Configurations with Fluent API

--- NLayerApp.Repository/AppDbContext.cs
+++ NLayerApp.Repository/AppDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayerApp.Core;
+using NLayerApp.Repository.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,5 +20,11 @@ namespace NLayerApp.Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        }
     }
 }
