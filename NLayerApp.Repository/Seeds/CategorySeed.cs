534fcf70b438b727572c51cf764bad46e398085a
= <=>
2022-04-26 23:59:39 +0300

    Add AutoMapper and MapProfile

--- NLayerApp.Repository/Seeds/CategorySeed.cs
+++ NLayerApp.Repository/Seeds/CategorySeed.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using NLayerApp.Core;
+using NLayerApp.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
