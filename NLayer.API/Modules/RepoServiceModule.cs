a1d27e24d4a8108b711d561aefff9857fd0b030c
= <=>
2022-05-02 00:57:10 +0300

    Add IoC container for repo and service layers

---
+++ NLayer.API/Modules/RepoServiceModule.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using NLayerApp.Core.Repositories;
+using NLayerApp.Core.Services;
+using NLayerApp.Core.UnitOfWorks;
+using NLayerApp.Repository;
+using NLayerApp.Repository.Repositories;
+using NLayerApp.Repository.UnitOfWorks;
+using NLayerApp.Service.Mapping;
+using NLayerApp.Service.Services;
+using System.Reflection;
+using Module = Autofac.Module;
+namespace NLayer.API.Modules
+{
+    public class RepoServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerMatchingLifetimeScope();
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerMatchingLifetimeScope();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+
+            var apiAssembly = Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            //InstancePerMatchingLifetimeScope => Scope'a denk gelir
+            //InstancePerDependency => Transient'e denk gelir
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+        }
+    }
+}
--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,18 +1,15 @@
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using System.Reflection;
 using NLayer.API.Filters;
 using NLayer.API.Middlewares;
-using NLayerApp.Core.Repositories;
-using NLayerApp.Core.Services;
-using NLayerApp.Core.UnitOfWorks;
 using NLayerApp.Repository;
-using NLayerApp.Repository.Repositories;
-using NLayerApp.Repository.UnitOfWorks;
 using NLayerApp.Service.Mapping;
-using NLayerApp.Service.Services;
 using NLayerApp.Service.Validations;
-using System.Reflection;
+using NLayer.API.Modules;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,17 +25,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
 builder.Services.AddAutoMapper(typeof(MapProfile));
 
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
-
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
@@ -47,6 +35,10 @@ builder.Services.AddDbContext<AppDbContext>(x =>
          options.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
      });
 });
+
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
 
 var app = builder.Build();
 
